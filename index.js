4d854a5ca574a93f1e052a8c216825526b94c68b
Dev <devmistry932@gmail.com>
2023-09-16 16:36:56 +0530

    mongodb url changes

--- index.js
+++ index.js
@@ -1,43 +1,47 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
-const mongoose = require("mongoose");
-const cors = require("cors");
-// routes import starts from here
-const adminRoutes = require("./admin/admin.routes");
-const userRoutes = require("./user/user.route")
-const workout = require("./Workout/workout.route")
 
-/// default middlewares
+// Import routes
+const adminRoutes = require('./admin/admin.routes');
+const userRoutes = require('./user/user.route');
+const workoutRoutes = require('./Workout/workout.route');
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: '*' }));
 
-/// routes starts from here
-app.use("/admin", adminRoutes);
-app.use("/user", userRoutes);
-app.use("/workout", workout);
+// Routes
+app.use('/admin', adminRoutes);
+app.use('/user', userRoutes);
+app.use('/workout', workoutRoutes);
 
 const port = process.env.PORT || 7000;
+const mongoURI =
+  process.env.MONGO_URI ||
+  // 'mongodb+srv://anas:anascr7@anas.qjhcajd.mongodb.net/?retryWrites=true&w=majority';
+  'mongodb+srv://minor-project:project123@cluster0.zsj7ci4.mongodb.net/';
 
-// mongoose.connect(
-//   // `mongodb+srv://anas:anascr7@anas.qjhcajd.mongodb.net/`,
-//   `mongodb+srv://anas:anascr7@anas.qjhcajd.mongodb.net/?retryWrites=true&w=majority`,
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   }
-// );
-mongoose.connect(
-  `mongodb+srv://anas:anascr7@anas.qjhcajd.mongodb.net/?retryWrites=true&w=majority`
-);
-
+// Connect to MongoDB
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-  console.log("Connected successfully");
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected successfully to MongoDB');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
 });
 
 app.listen(port, () => {
-  console.log(`server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
