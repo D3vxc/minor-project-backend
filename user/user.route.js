2f0bd289abe370c7b61f5876c9ad7067c74ab105
Dev <devmistry932@gmail.com>
2024-03-18 01:01:37 +0530

    user routes and controller changes

--- user/user.route.js
+++ user/user.route.js
@@ -1,10 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { register, login, getUsers } = require('./user.controller');
+const {
+  register,
+  login,
+  getUsers,
+  deleteUser,
+  forget_password,
+} = require("./user.controller");
 
-router.post('/register', register);
-router.post('/login', login);
-router.get('/getusers', getUsers);
-
+router.post("/register", register);
+router.post("/login", login);
+router.get("/getusers", getUsers);
+router.delete("/deleteuser/:id", deleteUser);
+router.post("/forget_password", forget_password);
 
 module.exports = router;
