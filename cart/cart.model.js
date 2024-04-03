b0be499220a4769283f98dcdd9de3d96645921db
Dev <devmistry932@gmail.com>
2024-04-03 15:38:06 +0530

    cart schema

--- cart/cart.model.js
+++ cart/cart.model.js
@@ -1,40 +1,29 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const CartItemSchema = new Schema(
-  {
-    product: {
-      type: Schema.Types.ObjectId,
-      ref: "Product",
-      required: true,
-    },
-    quantity: {
-      type: Number,
-      required: true,
-      min: 1,
-    },
+const cartItemSchema = new Schema({
+  product: {
+    type: Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
   },
-  { _id: false }
-);
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+});
 
-const CartSchema = new Schema(
+const cartSchema = new Schema(
   {
     user: {
       type: Schema.Types.ObjectId,
       ref: "User",
       required: true,
     },
-    items: [CartItemSchema],
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
-    updatedAt: {
-      type: Date,
-      default: Date.now,
-    },
+    productData: [cartItemSchema],
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("Cart", CartSchema);
+module.exports = mongoose.model("Cart", cartSchema);
